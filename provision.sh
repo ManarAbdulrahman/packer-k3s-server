32bee79c917f8b793a21a372277c180622d70100
Omar Bazaid <omar.vip3@gmail.com>
2020-08-28 18:51:24 +0300

    remove k3s second installation

--- provision.sh
+++ provision.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt update -y && sudo apt install -y curl
 sudo apt update -y && sudo apt install -y vim make git
-curl -sfL https://get.k3s.io | sh -
 curl -L get.docker.com | sh
 sudo usermod -aG docker ubuntu
 curl -sfL https://get.k3s.io | sh -
