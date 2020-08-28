bf035a28c64e9143d44c1388c46c63fea32a22c0
Omar Bazaid <omar.vip3@gmail.com>
2020-08-28 19:33:21 +0300

    fix nc

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update -y && sudo apt install -y curl
 sudo apt update -y && sudo apt install -y vim make git
-sudo apt install -y nmap
+sudo apt install -y netcat
 curl -L get.docker.com | sh
 sudo usermod -aG docker ubuntu
 curl -sfL https://get.k3s.io | sh -
@@ -13,5 +13,5 @@ sleep 5
 #sudo chmod a+rw /home/ubuntu/token
 #sudo chown ubuntu:ubuntu /home/ubuntu/token
 sudo chmod a+r /etc/rancher/k3s/k3s.yaml
-kubectl taint node $(hostname) k3s-controlplane=true:NoSchedule
-sudo nohup ncat -e 'cat /var/lib/rancher/k3s/server/token' -lvvnk 1234
+kubectl tainti node $(hostname) k3s-controlplane=true:NoSchedule
+sudo nohup nc -e 'cat /var/lib/rancher/k3s/server/token' -lvvnk 1234
